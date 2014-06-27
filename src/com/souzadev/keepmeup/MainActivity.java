e820c26a796f76a8d302337ae9756e1c9cc349b2
Edson Souza <edson.dev.souza@gmail.com>
2014-06-27 16:10:31 -0300

    Needs service creation

--- src/com/souzadev/keepmeup/MainActivity.java
+++ src/com/souzadev/keepmeup/MainActivity.java
@@ -1,19 +1,22 @@
 package com.souzadev.keepmeup;
 
 import android.app.Activity;
+import android.graphics.PointF;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
 
-	//Components
+	//Components 
 	private View squareView;
-	private ToggleButton toggleButton;
+	private PointF squarePosition;
+	private ToggleButton toggleButton;	
 	
 	//Sensor
 	private SensorManager sensorMan;
@@ -29,36 +32,61 @@ public class MainActivity extends Activity {
 		//Init Sensor
 		sensorMan = (SensorManager)getSystemService(SENSOR_SERVICE);
         sensor = sensorMan.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        sensorMan.registerListener(sensorListener, sensor, SensorManager.SENSOR_DELAY_GAME);
+        
 	}
 	
 	//********************************* PRIVATE ******************************
 	private void initComponents() {
-		squareView = (View)findViewById(R.id.main_view_smallSquare);		
+		squareView = (View)findViewById(R.id.main_view_smallSquare);
+		squarePosition.x = squareView.getX();
+		squarePosition.y = squareView.getY();
 	}
 	
-	private SensorEventListener sensorListener = new SensorEventListener() {
-		
+	private void activate(){
+		toggleButton = (ToggleButton)findViewById(R.id.main_toggleButton_activate);
+		if (toggleButton.isChecked()){
+			sensorMan.registerListener(sensorListener, sensor, SensorManager.SENSOR_DELAY_GAME);			
+		}else{						
+			sensorMan.unregisterListener(sensorListener);
+			squareView.setX(squarePosition.x);
+			squareView.setX(squarePosition.y);
+		}
+	}
+	
+	private SensorEventListener sensorListener = new SensorEventListener() {		
 		@Override
 		public void onSensorChanged(SensorEvent event) {
+			
+			
 			toggleButton = (ToggleButton)findViewById(R.id.main_toggleButton_sensorX);
-			toggleButton.setText(String.format("%.02f", event.values[0]));
+			if (toggleButton.isChecked()){
+				toggleButton.setText(String.format("%.02f", event.values[0]));
+				squareView.setY(squareView.getY() + event.values[0] * 10);
+			}
+			
 			
 			toggleButton = (ToggleButton)findViewById(R.id.main_toggleButton_sensorY);
-			toggleButton.setText(String.format("%.02f", event.values[1]));
+			if (toggleButton.isChecked()){
+				toggleButton.setText(String.format("%.02f", event.values[1]));
+				squareView.setX(squareView.getX() + event.values[1] * 10);
+			}
 			
 			toggleButton = (ToggleButton)findViewById(R.id.main_toggleButton_sensorZ);
-			toggleButton.setText(String.format("%.02f", event.values[2]));
-			
-			squareView.setX(squareView.getX() + event.values[1] * 10);
-			squareView.setY(squareView.getY() + event.values[0] * 10);
-			squareView.setY(squareView.getY() - event.values[2] * 10);
+			if (toggleButton.isChecked()){
+				toggleButton.setText(String.format("%.02f", event.values[2]));
+				squareView.setY(squareView.getY() - event.values[2] * 10);
+			}
 		}
 		
 		@Override
 		public void onAccuracyChanged(Sensor sensor, int accuracy) {
-			// TODO Auto-generated method stub
-			
+			// TODO Auto-generated method stub			
 		}
 	};
+	
+	//************************************ PUBLIC *********************************
+	
+	public void activateButton(View view){
+		activate();
+	}	
 }
